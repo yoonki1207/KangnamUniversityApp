1825b50e9a5bfe591de2adb5b0da9eb071e2e30f
yoonki1207 <yoonki1207@naver.com>
2020-02-02 23:07:15 +0900

    day1 at DESKTOP

--- app/src/main/java/com/example/kangnamuniversityapp/NoticeListAdaptor.java
+++ app/src/main/java/com/example/kangnamuniversityapp/NoticeListAdaptor.java
@@ -16,12 +16,16 @@ public class NoticeListAdaptor extends RecyclerView.Adapter<NoticeListAdaptor.My
     Context mContext = null;
     LayoutInflater mLayoutInflater = null;
     ArrayList<ArticleInfo> articles;
+    // RecyclerView.ViewHolder를 상속해서 말 그래돌 뷰 홀더를 만든다.
+    // 이곳에는 리스트 각 목록에 해당하는 요소들 (title, date, views 등)을 묶는다.
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public View view;
+        TextView titleView, timeView, typeView;
         public MyViewHolder(View v) {
             super(v);
-            view = v;
+            titleView = v.findViewById(R.id.text_view_title);
+            timeView = v.findViewById(R.id.text_view_time);
+            typeView = v.findViewById(R.id.text_view_type);
         }
     }
     public NoticeListAdaptor(Context context, ArrayList<ArticleInfo> data){
@@ -31,7 +35,7 @@ public class NoticeListAdaptor extends RecyclerView.Adapter<NoticeListAdaptor.My
     }
 
     @NonNull
-    @Override
+    @Override  //  onCreateViewHolder는 ViewHolder가 만들어질때마다 생성되고 이는 ViewHolder를 반환해야 한다.
     public NoticeListAdaptor.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         // create a new view
         View v = (View) LayoutInflater.from(viewGroup.getContext())
@@ -41,7 +45,7 @@ public class NoticeListAdaptor extends RecyclerView.Adapter<NoticeListAdaptor.My
         return vh;
     }
 
-    @Override
+    @Override  //  항목을 설정set 하는 곳.
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int position) {
 //        View v = mLayoutInflater.inflate(R.layout.notice_content_simple, null);
 //        TextView titleView = (TextView)v.findViewById(R.id.textViewTitle);
@@ -51,9 +55,13 @@ public class NoticeListAdaptor extends RecyclerView.Adapter<NoticeListAdaptor.My
 //        titleView.setText(articles.get(position).getTitle());
 //        timeView.setText(articles.get(position).getTime());
 //        typeView.setText(articles.get(position).getType());
-        ((TextView)myViewHolder.view.findViewById(R.id.textViewTitle)).setText(articles.get(position).getTitle());
-        ((TextView)myViewHolder.view.findViewById(R.id.textViewTime)).setText(articles.get(position).getTime());
-        ((TextView)myViewHolder.view.findViewById(R.id.textViewType)).setText(articles.get(position).getType());
+//        ((TextView)myViewHolder.view.findViewById(R.id.textViewTitle)).setText(articles.get(position).getTitle());
+//        ((TextView)myViewHolder.view.findViewById(R.id.textViewTime)).setText(articles.get(position).getTime());
+//        ((TextView)myViewHolder.view.findViewById(R.id.textViewType)).setText(articles.get(position).getType());
+        ArticleInfo article = articles.get(position);
+        myViewHolder.titleView.setText(article.getTitle());
+        myViewHolder.timeView.setText(article.getTime());
+        myViewHolder.typeView.setText(article.getType());
     }
 
     @Override

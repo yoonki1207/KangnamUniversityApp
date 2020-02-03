0edbf257492fdc8373f5a7e1e07e03340ce2ec6e
yoonki1207 <yoonki1207@naver.com>
2020-02-03 18:24:10 +0900

    Refresh구현하는데 오류남

--- app/src/main/java/com/example/kangnamuniversityapp/MainActivity.java
+++ app/src/main/java/com/example/kangnamuniversityapp/MainActivity.java
@@ -24,62 +24,33 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
 
-    ArrayList<ArticleInfo> noticeData;
-    final ArrayList<ArticleInfo> noticeDataTmp = new ArrayList<ArticleInfo>();
+    ArrayList<ArticleInfo> noticeData = new ArrayList<ArticleInfo>();
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notice);
         final String url = "https://web.kangnam.ac.kr/menu/f19069e6134f8f8aa7f689a4a675e66f.do";
+        final String url2 = "https://m.cafe.naver.com/ArticleListAjax.nhn?search.clubid=27842958&search.menuid=1&search.page=1";
 
+        swipeRefreshLayout = findViewById(R.id.notice_swipe_refresh_layout);
+        final Thread noticeArticleParsingThread = loadUkkikki(url2);//url주소에서 article 긁어오는 쓰레드 생성
+        final Thread thread = loadUkkikki(url2);
+        //thread.start();
 
+        noticeArticleParsingThread.start();// 쓰레드 시작
+        try {
+            noticeArticleParsingThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        recyclerView = (RecyclerView)findViewById(R.id.notice_article_recyclerView);
 
-        Thread noticeArticleParsingThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Document document = WebRequestBuilder.create()
-                            .url(url)
-                            .method(WebRequestBuilder.METHOD_GET)
-                            .userAgent(WebRequestBuilder.USER_AGENT_PC)
-                            .useCookie(true)
-                            .build();//Jsoup.connect(url).get();
-
-                    Elements listArticle = document.selectFirst("div[class=tbody]").select("ul");
-                    Log.e("Dsds", "count: " + listArticle.size());
-
-                    for (Element article : listArticle) {
-                        String title, author, num, time, type, file, views;
-                        Elements listContent = article.select("li");
-
-
-                        num = listContent.get(0).text();
-                        type = listContent.get(1).text();
-                        title = listContent.get(2).text();
-                        author = listContent.get(4).text();
-                        time = listContent.get(5).text();
-                        file = "none";
-                        views = "none";
-
-                        noticeDataTmp.add(new ArticleInfo(num, type, title, file, author, time, views));
-                        //널오류났었음^
-                        Log.d("num : ", "["+num+"]");
-                        Log.d("type : ", type);
-                        Log.d("title : ", title);
-                        Log.d("author : ", author);
-                        Log.d("time : ", time);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
         ItemClickSupport.addTo(recyclerView).setOnItemClickListener(
                 new ItemClickSupport.OnItemClickListener() {
                     @Override
@@ -88,16 +59,6 @@ public class MainActivity extends AppCompatActivity{
                     }
                 }
         );
-        noticeArticleParsingThread.start();
-
-        try {
-            noticeArticleParsingThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        //this.InitializeNoticeArticleData();
-        recyclerView = (RecyclerView)findViewById(R.id.notice_article_recyclerView);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -108,16 +69,98 @@ public class MainActivity extends AppCompatActivity{
         recyclerView.setLayoutManager(layoutManager);
 
         //어댑터 명시
-        mAdapter = new NoticeListAdaptor(this, noticeDataTmp);
+        mAdapter = new NoticeListAdaptor(this, noticeData);
         recyclerView.setAdapter(mAdapter);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {//새로고침 하면
+
+                if(noticeArticleParsingThread.isAlive()==false){
+                    noticeArticleParsingThread.start();
+                }
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
 
     }
 
-    public void InitializeNoticeArticleData() {
+    public void InitializeNoticeArticleData() {// sample data
         noticeData = new ArrayList<ArticleInfo>();
 
         noticeData.add(new ArticleInfo("1336", "장학", "국가장학금신청", "첨부파일", "장학복지팀", "20.01.30", "1054"));
         noticeData.add(new ArticleInfo("1337", "장학", "국가장학금신청2", "첨부파일", "장학복지팀", "20.01.31", "941"));
     }
 
+    private Thread loadNoticeArticle(final String url){
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document document = WebRequestBuilder.create()
+                            .url(url)
+                            .method(WebRequestBuilder.METHOD_GET)
+                            .userAgent(WebRequestBuilder.USER_AGENT_PC)
+                            .useCookie(true)
+                            .build();//Jsoup.connect(url).get();
+
+                    Elements listArticle = document.selectFirst("div[class=tbody]").select("ul");// html tag 전부 찾아서 Elments로 만듦.
+                    Log.e("Dsds", "count: " + listArticle.size());
+
+                    for (Element article : listArticle) {
+                        String title, author, num, time, type, file, views;
+                        Elements listContent = article.select("li");
+
+                        num = listContent.get(0).text();
+                        type = listContent.get(1).text();
+                        title = listContent.get(2).text();
+                        author = listContent.get(4).text();
+                        time = listContent.get(5).text();
+                        file = "none";
+                        views = "none";
+                        noticeData.clear();
+                        noticeData.add(new ArticleInfo(num, type, title, file, author, time, views));
+                        //널오류났었음^
+//                        Log.d("num : ", "["+num+"]");
+//                        Log.d("type : ", type);
+//                        Log.d("title : ", title);
+//                        Log.d("author : ", author);
+//                        Log.d("time : ", time);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    } //
+    private Thread loadUkkikki(final String url){
+        Log.d(",","asd");
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Document document = WebRequestBuilder.create()
+                            .url(url)
+                            .method(WebRequestBuilder.METHOD_GET)
+                            .userAgent(WebRequestBuilder.USER_AGENT_PC)
+                            .useCookie(true)
+                            .build();//Jsoup.connect(url).get();
+                    Elements listArticle = document.select("li[class=board_box]");
+                    for(Element article : listArticle){
+                        String title, author, time;
+                        Element listContent = article.selectFirst("strong[class=tit]");
+                        Log.d("우끼ㅣ끼끼ㅣ끼끼",listContent.text());
+                        title = listContent.text();
+                        author = article.selectFirst("span[class=ellip]").text();
+                        time = article.selectFirst("span[class=time]").text();
+                        noticeData.clear();
+                        noticeData.add(new ArticleInfo("num", "type", title, "file", author, time, "views"));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
